8937078a056df33a4bd698300fa044639e724c2c
ChinoDFO <a22300864@ceti.mx>
2026-01-29 20:41:54 -0600

    Versión casi completa 2

--- src/services/openaiService.js
+++ src/services/openaiService.js
@@ -1,7 +1,7 @@
 // src/services/openaiService.js
 import axios from 'axios';
 
-const API_URL = 'https://ready-to-cook.vercel.app/api/openai';
+const API_URL = 'https://ready-to-cook.vercel.app/Api/index.js';
 
 export const generateRecipe = async ({
   ingredients,
