a7590b99f816e60db2047e022475414164796689
ChinoDFO <a22300864@ceti.mx>
2026-01-29 21:23:18 -0600

    Ya subete por favorrr 3

--- src/app/api/openai/route.js
+++ src/app/api/openai/route.js
@@ -9,7 +9,7 @@ export async function POST(req) {
       {
         method: 'POST',
         headers: {
-          Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+          'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`,
           'Content-Type': 'application/json'
         },
         body: JSON.stringify(body)
